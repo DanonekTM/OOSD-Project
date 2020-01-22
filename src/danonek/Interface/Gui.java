f287ce0708ff22987a1b10ac4b32cc5e00e8ee6b
Wasyl <58756162+W4syl@users.noreply.github.com>
2020-01-22 22:19:04 +0000

    Update Gui.java

--- src/danonek/Interface/Gui.java
+++ src/danonek/Interface/Gui.java
@@ -1,6 +1,10 @@
 package danonek.Interface;
 import javax.swing.*;
 import java.awt.*;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
 
 public class Gui 
 {
@@ -9,43 +13,79 @@ public class Gui
 		//Creating the Frame
 		JFrame frame = new JFrame("Test Frame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(600, 800);
+		frame.setSize(800, 600);
+		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(60dlu;default)"),
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel CustomerNameLabel = new JLabel("Customer Name :");
+		frame.getContentPane().add(CustomerNameLabel, "4, 4");
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		frame.getContentPane().add(lblNewLabel_4, "28, 4");
+		
+		JLabel lblNewLabel_1 = new JLabel("Customer Adress :");
+		frame.getContentPane().add(lblNewLabel_1, "4, 10");
+		
+		JLabel lblNewLabel_5 = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel_5, "28, 10");
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel_2, "4, 16");
+		
+		JLabel lblNewLabel_6 = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel_6, "28, 16");
+
 	
-		//Creating the MenuBar and adding components
-		JMenuBar mb = new JMenuBar();
-		JMenu m1 = new JMenu("FILE");
-		JMenu m2 = new JMenu("Help");
-		mb.add(m1);
-		mb.add(m2);
-		JMenuItem m11 = new JMenuItem("Open");
-		JMenuItem m22 = new JMenuItem("Save as");
-		m1.add(m11);
-		m1.add(m22);
-	
-		//Creating the panel at bottom and adding components
-		JPanel panel = new JPanel(); // the panel is not visible in output
-		JLabel label = new JLabel("Enter Text");
-		JTextField tf = new JTextField(10); // accepts upto 10 characters
-		JButton create = new JButton("Create");
-		JButton retrieve = new JButton("Retrieve");
-		JButton update = new JButton("Update");
-		JButton reset = new JButton("delete");
-		panel.add(label); // Components Added using Flow Layout
-		panel.add(label); // Components Added using Flow Layout
-		panel.add(tf);
-		panel.add(create);
-		panel.add(retrieve);
-		panel.add(update);
-		panel.add(reset);
-	
-	
-		// Text Area at the Center
-		JTextArea ta = new JTextArea();
-	
-		//Adding Components to the frame.
-		frame.getContentPane().add(BorderLayout.SOUTH, panel);
-		frame.getContentPane().add(BorderLayout.NORTH, mb);
-		frame.getContentPane().add(BorderLayout.CENTER, ta);
 		frame.setVisible(true);
 	}//body
 }//class
